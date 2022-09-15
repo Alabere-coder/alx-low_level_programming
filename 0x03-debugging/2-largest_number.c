596bbf1765afbf28eb4bbe133265fac74d4894a6
Alabere-coder <alabere4real247@gmail.com>
2022-09-15 07:12:05 -0700

    This program prints the largest of three integers.

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * largest_number - returns the largest of 3 numbers
@@ -10,7 +10,7 @@
 
 int largest_number(int a, int b, int c)
 {
-	int largest = b;
+	int largest;
 
 	if (a > c && a > c)
 	{
